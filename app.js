0e17204722cb0c3b48bd04b9ba901d625721cb95
trsalondra <trsalondra@gmail.com>
2022-11-07 17:41:20 -0600

    change event listners

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-// I got it to work! I just had to call both functions stats before using the  buttons
+///////////////////////////////////////////////////////////
+// OBJECTS
+///////////////////////////////////////////////////////////
+//#region
+
+let index = 0
 
 const player = {
     hull: 20,
@@ -8,10 +13,16 @@ const player = {
     attack(enemy) {
         if (Math.random() < player.accuracy) {
             enemy.hull -= player.firepower
-            console.log(`HIT! Updated Enemy Hull: ${enemy.hull}`)
-            return 'Congratulations!<br>Your laser attack HIT the Enemy!'
+            if (enemy.hull <= 0) {
+                enemy.isAlive = false
+                console.log(`Congratulations! The Enemy Ship has be destroyed`)
+                return `Congratulations!<br> The Enemy Ship has be destroyed`
+            } else {
+                console.log(`Congratulations! Your laser HIT the Enemy Ship`)
+                return `Congratulations!<br>Your laser HIT Enemy Ship`
+            }
         } else {
-            console.log(`MISSED! Enemy Hull: ${enemy.hull}`)
+            console.log('Unfortunately! Your laser attack MISSED the Enemy')
             return 'Unfortunately!<br>Your laser attack MISSED the Enemy'
         }
     }
@@ -20,108 +31,157 @@ const player = {
 class EnemyShip {
     constructor(index) {
         this.num = index,
-        this.hull = Math.floor(Math.random() * 3) + 18,
-        this.firepower = Math.floor(Math.random() * 3) + 2,
-        this.accuracy = eval((Math.random() * .2 + .6).toFixed(2)),
-        this.isAlive = true
+            this.hull = Math.floor(Math.random() * 3) + 7,
+            this.firepower = Math.floor(Math.random() * 3) + 2,
+            this.accuracy = eval((Math.random() * .2 + .6).toFixed(2)),
+            this.isAlive = true
     }
     attack(player) {
         if (Math.random() < this.accuracy) {
             player.hull -= this.firepower
-            console.log(`HIT! Upated Player Hull: ${player.hull}`)
-            return "Unfortunately!<br>The Enemy's laser attack HIT YOU!"
-        } else {
-            console.log(`MISSED! Player Hull: ${player.hull}`)
-            return "Congratulations!<br>The Enemy's laser attack MISSED YOU"
+            if (player.hull <= 0) {
+                player.isAlive = false
+                console.log(`GAME OVER! You LOSE!`)
+                return `GAME OVER! You LOSE!`
+            } else {
+                console.log("The Enemy's laser HIT YOU!")
+                return "The Enemy's laser HIT YOU!"
+            }
         }
     }
 }
+//#endregion
 ///////////////////////////////////////////////////////////
 // ENEMY FACTORY
 ///////////////////////////////////////////////////////////
-
-let enemyNum = 6
+//#region
+let enemyNum = 4
 let enemy = []
 
 for (let i = 0; i < enemyNum; i++) {
     enemy.push(new EnemyShip(i))
 }
 
-console.log(enemy)
+//#endregion
 ///////////////////////////////////////////////////////////
 // STATS
 ///////////////////////////////////////////////////////////
-
-let enemyIndex = document.querySelector('#enemyIndex')
-
-enemyIndex.innerHTML = ` ${eval(enemy[0].num + 1)}`
-
+//#region
 let playerStats = document.querySelector('.playerStats')
 
-playerStats.innerHTML = `Hull : ${player.hull} <br>
-FirePower : ${player.firepower} <br>
-Accuracy : ${player.accuracy} <br>`
+displayStats(playerStats, player)
 
 let enemyStats = document.querySelector('.enemyStats')
 
-function displaySats (stats, player) {
+displayStats(enemyStats, enemy)
+
+function displayStats(stats, player) {
     stats.innerHTML = `Hull : ${player.hull} <br> 
     FirePower : ${player.firepower} <br>
     Accuracy : ${player.accuracy} <br>`
 }
-
-displaySats(enemyStats, enemy[0])
-
-// enemyStats.innerHTML = `Hull : ${enemy[0].hull} <br> 
-// FirePower : ${enemy[0].firepower} <br>
-// Accuracy : ${enemy[0].accuracy} <br>`
+//#endregion
 ///////////////////////////////////////////////////////////
 // MOD BUTTON
 ///////////////////////////////////////////////////////////
-let btn = document.querySelector('#btn')
+//#region
+let playBtn = document.querySelector('#playBtn')
 
 let mgsModal = document.querySelector('#msgModal')
 
 let msg = document.querySelector('#msg')
 
-btn.onclick = function () {
-    switch (btn.innerText) {
-        case 'START':
-            msg.innerHTML = "<p>Earth has been attacked by a horde of aliens! You are the captain of the USS HelloWorld, on a mission to destroy every last alien ship.</p><br><p>Battle the aliens as you try to destroy them with your lasers.</p><br><p>There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.</p>"
-            break
-        case 'Attack':
-            msg.innerHTML = player.attack(enemy[0])
-            displaySats(enemyStats, enemy[0])
-            break
+let closeMsg = document.querySelector('#closeMsg')
 
-        case 'Await Attack':
-            msg.innerHTML = enemy[0].attack(player)
-            displaySats(playerStats, player)
+///////////////////////////////////////////////////////////
+
+function updateEnemy(enemyArr) {
+    return enemyArr.filter(element => element.isAlive === true)
+}
+
+let updatedEnemy = []
+
+updatedEnemy = updateEnemy(enemy)
+console.log(updatedEnemy)
+
+playBtn.onclick = function () {
+
+    // problem is that is goes to await attack for the same ship that has been destroyed
+
+    switch (playBtn.innerText) {
+        case 'Attack':
+            console.log(`${index + 1} attack case`)
+
+            msg.innerHTML = player.attack(updatedEnemy[index])
+            if (updatedEnemy[index].isAlive === true) {
+                playBtn.innerHTML = 'Await Attack'
+                playBtn.style.color = 'red'
+            } else {
+            index++
+            enemyIndex.innerHTML = `&nbsp;${enemy[index].num + 1}`
+            playBtn.innerHTML = 'Attack'
+            playBtn.style.color = 'green'
+            
+            }
+            displayStats(enemyStats, updatedEnemy[index])
+            console.log(updatedEnemy)
             break
+        case 'Await Attack':
+            console.log(`${index + 1} await case `)
+            msg.innerHTML = updatedEnemy[index].attack(player)
+            if (player.isAlive === true){
+                index++
+                enemyIndex.innerHTML = `&nbsp;${enemy[index].num + 1}`
+                playBtn.innerHTML = 'Attack'
+                playBtn.style.color = 'green'
+            }else {
+                console.log('player lost')
+            }
+            displayStats(playerStats, player)
+            console.log(updatedEnemy)
+            break
+    }
+
+    if (index === updatedEnemy.length) {
+        console.log('restart')
+        updatedEnemy = updateEnemy(enemy)
+        console.log(updatedEnemy)
+        index = 0
+        if (updateEnemy.length === 0) {
+            console.log('player won')
+        }
     }
     mgsModal.style.display = 'block'
 }
+
 /////////////////////////////////////////////////////////// 
-// CLOSING MOD
+// CLOSING
 ///////////////////////////////////////////////////////////
-let closeMsg = document.querySelector('#closeMsg')
 
 closeMsg.onclick = function () {
-    mgsModal.style.display = 'none'
+    msgModal.style.display = 'none'
+    enemyIndex.style.display = 'block'
+    // switch (playBtn.innerText) {
+    //     case 'PLAY':
+    //         playBtn.innerHTML = 'Attack'
+    //         playBtn.style.color = 'green'
+    //     case 'Attack':
+    //         if(updatedEnemy[index].isAlive === true){
+    //             playBtn.innerHTML = 'Await Attack'
+    //             playBtn.style.color = 'red'
 
-    switch (btn.innerText) {
-        case 'START':
-            btn.innerHTML = 'Attack'
-            btn.style.color = 'green'
-            enemyIndex.style.display = 'block'
-            break
-        case 'Attack':
-            btn.innerHTML = 'Await Attack'
-            btn.style.color = 'red'
-            break
-        case 'Await Attack':
-            btn.innerHTML = 'Attack'
-            btn.style.color = 'green'
-            break
-    }
+    //         } else {
+    //             index++
+    //             enemyIndex.innerHTML = `&nbsp;${enemy[index].num + 1}`
+    //             playBtn.innerHTML = 'Attack'
+    //             playBtn.style.color = 'green'
+    //         }
+    //     case 'Await Attack':
+    //         enemyIndex.innerHTML = `&nbsp;${enemy[index].num + 1}` // insert +1 later
+    //         playBtn.innerHTML = 'Attack'
+    //         playBtn.style.color = 'green'
+    // }
 }
+
+console.log(`${enemy[index].num + 1}`)
+
